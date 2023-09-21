87846778278c82b279a3df666efe79e560198653
Simon Lynch <simon.lynch@hashicorp.com>
2023-09-21 17:14:31 +1000

    example WAF

---
+++ terraform-aws-wafv2-webacl/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  # Configuration options
+  region = var.region
+}
+
+resource "aws_wafv2_web_acl" "example" {
+  name        = var.web_acl_name
+  description = "Example of a managed rule."
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "rule-1"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+
+        rule_action_override {
+          action_to_use {
+            count {}
+          }
+
+          name = "NoUserAgent_HEADER"
+        }
+
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  tags = {
+    Tag1 = "Value1"
+    Tag2 = "Value2"
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
+
---
+++ terraform-aws-wafv2-webacl/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "web_acl_name" {
+    type = string
+    default = "some_web_acl"
+}
