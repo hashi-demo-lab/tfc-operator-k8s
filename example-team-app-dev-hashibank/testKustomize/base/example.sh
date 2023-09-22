ce0831c0604bf6ed7b86be1ba298082695c4cd6e
Simon Lynch <simon.lynch@hashicorp.com>
2023-09-22 13:29:43 +1000

    update testing

--- example-team-app-dev-hashibank/testKustomize/base/example.sh
+++ example-team-app-dev-hashibank/testKustomize/base/example.sh
@@ -1,1 +1,1 @@
-WAF_ARN_VALUE=$(kubectl get configmap module-hashibank-aws-waf-outputs -o=jsonpath='{.data.arn}') > ./base/environment.env
+echo WAF_ARN_VALUE=$(kubectl get configmap module-hashibank-aws-waf-outputs -o=jsonpath='{.data.arn}') > ./environment.env
