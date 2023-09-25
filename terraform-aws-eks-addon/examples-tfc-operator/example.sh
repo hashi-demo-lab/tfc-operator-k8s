abdbf5eb84997463acdc434445b38402551aaf74
Simon Lynch <simon.lynch@hashicorp.com>
2023-09-25 15:20:20 +1000

    update script

--- terraform-aws-eks-addon/examples-tfc-operator/example.sh
+++ terraform-aws-eks-addon/examples-tfc-operator/example.sh
@@ -3,9 +3,9 @@ doormat login -f && eval $(doormat aws export --account ${DOORMAT_AWS_USER})
 VARSET=varset-UTad3ixYVAvo41W9
 doormat aws tf-push variable-set --id $VARSET --role arn:aws:iam::855831148133:role/aws_simon.lynch_test-developer
 
-
-
 kubectl config use-context docker-desktop
+
+#namespace
 NAMESPACE="app-hashibank"
 kubectl config set-context --current --namespace="$NAMESPACE"
 
@@ -15,23 +15,7 @@ kubectl get workspace ws-module-eks-addon -o yaml --watch
 
 kubectl get configmap ws-module-eks-addon-outputs -o yaml
 
-
-# Get all pods in the namespace and store them in an array
-#POD_ARRAY=($(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}'))
-
-# Print the pods to verify
-#for POD in "${POD_ARRAY[@]}"; do
-#    echo "Pod: $POD"
-#    kubectl logs -f -n $NAMESPACE $POD &
-#done
-
-# Wait for all background log tailing processes
-#wait
-
-
-#kubectl get configmap platform-eks-fargate-env1-module-outputs -o yaml
-
 #To force run
-#kubectl patch module platform-eks-fargate-env1 --type=merge --patch '{"spec": {"restartedAt": "'`date -u -Iseconds`'"}}'
+#kubectl patch module module-eks-addon --type=merge --patch '{"spec": {"restartedAt": "'`date -u -Iseconds`'"}}'
 
-#exit
+
