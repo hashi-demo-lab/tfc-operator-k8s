ee25d874aaa49397a8175a98657d0346062b3d69
Simon Lynch <simon.lynch@hashicorp.com>
2023-09-26 15:40:50 +1000

    update app with job template

---
+++ example-team-app-dev-hashibank/example.sh
@@ -0,0 +1,34 @@
+#!usr/bin/zsh
+doormat login -f && eval $(doormat aws export --account ${DOORMAT_AWS_USER})
+VARSET=varset-UTad3ixYVAvo41W9
+doormat aws tf-push variable-set --id $VARSET --role arn:aws:iam::855831148133:role/aws_simon.lynch_test-developer
+
+cluster_name="eks-cluster"
+aws eks --region ap-southeast-2 update-kubeconfig --name $cluster_name
+
+namespace="app-hashibank"
+kubectl config set-context --current --namespace="$namespace"
+
+
+
+kubectl get module --watch
+
+# Get all pods in the namespace and store them in an array
+#POD_ARRAY=($(kubectl get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}'))
+
+# Print the pods to verify
+#for POD in "${POD_ARRAY[@]}"; do
+#    echo "Pod: $POD"
+#    kubectl logs -f -n $NAMESPACE $POD &
+#done
+
+# Wait for all background log tailing processes
+#wait
+
+
+#kubectl get configmap platform-eks-fargate-env1-module-outputs -o yaml
+
+#To force run
+#kubectl patch module platform-eks-fargate-env1 --type=merge --patch '{"spec": {"restartedAt": "'`date -u -Iseconds`'"}}'
+
+exit
